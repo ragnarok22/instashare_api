88b07c5a472cb17b8c20af7b8aa951536f353af4
Reinier Hernández <sasuke.reinier@gmail.com>
2022-12-01 12:42:19 -0500

    test: added test for registration

--- apps/accounts/tests/test_views.py
+++ apps/accounts/tests/test_views.py
@@ -149,3 +149,22 @@ class AccountTests(APITestCase):
                 "This password is too common.",
             ],
         )
+
+    def test_register_user_with_missing_email(self):
+        """Register a new user with short password"""
+        url = reverse("register")
+
+        data = {
+            "username": "john",
+            "first_name": "John",
+            "last_name": "Doe",
+            "password": "myPass123Ultra",
+            "password2": "myPass123Ultra",
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            json.loads(response.content).get("email"), ["This field is required."]
+        )
